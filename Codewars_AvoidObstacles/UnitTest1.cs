274e653ef0a36f412789119ca8f9cd61ae20a743
hatelove <jay.hatelove@gmail.com>
2017-03-21 13:59:39 +0800

    add the first failed test case: arr is {1}, should return 2

---
+++ Codewars_AvoidObstacles/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_AvoidObstacles
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Obstacles_are_1_min_legnth_should_be_2()
+        {
+            var arr = new int[] { 1 };
+            Assert.AreEqual(2, new Kata().AvoidObstacles(arr));
+        }
+    }
+
+    public class Kata
+    {
+        public int AvoidObstacles(int[] arr)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
